8a06e88e645feff2cd73e1c47e34897f607588b3
marlenipong <marleniponglisu@gmail.com>
2024-12-10 16:38:05 +0900

    first commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "leni.h"
+
+int main ()
+{
+
+  printf("\t\t Library sistem operasi\n");
+  printf("\n ============================== \n");
+  printf("Welcome to converter money");
+  printf("\n ============================== \n");
+  printf("\n nilai dolar ke rupiah \n");
+  rupiah();
+  
+  printf("\n nilai rupiah ke dolar \n");
+  dolar();
+}
